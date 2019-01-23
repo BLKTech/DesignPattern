6625880c4e3898ff6f58b8f47d096988e15e8070
blktech <github@blktech.com>
2019-01-23 05:04:37 +0000

    Auto Commit mié ene 23 05:04:37 UTC 2019

---
+++ Singleton.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ */
+
+namespace BLKTech\DesignPatterns;
+
+/**
+ * Singleton Pattern
+ *
+ * @author https://stackoverflow.com/questions/203336/creating-the-singleton-design-pattern-in-php5
+ * @author The Blankis < blankitoracing@gmail.com >
+ */
+
+abstract class Singleton
+{
+    private static $instances = array();
+    protected function __construct() {}
+    protected function __clone() {}
+    public function __wakeup() 
+    {
+        throw new Exception("Cannot unserialize singleton");
+    }
+
+    public static function getInstance()
+    {
+        $cls = get_called_class(); // late-static-bound class name
+        if (!isset(self::$instances[$cls])) 
+            self::$instances[$cls] = new static;
+        
+        return self::$instances[$cls];
+    }   
+}
