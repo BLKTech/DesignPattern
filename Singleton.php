2bf352c715bd34ece483f11f17e80009e0f8dd78
BLKTech Git Service <git@blktech.com>
2023-09-18 13:25:26 +0000

    php-cs-fixer

--- Singleton.php
+++ Singleton.php
@@ -26,9 +26,13 @@ namespace BLKTech\DesignPattern;
 abstract class Singleton
 {
     private static $instances = array();
-    protected function __construct() {}
-    protected function __clone() {}
-    public function __wakeup() 
+    protected function __construct()
+    {
+    }
+    protected function __clone()
+    {
+    }
+    public function __wakeup()
     {
         throw new Exception("Cannot unserialize singleton");
     }
@@ -36,9 +40,10 @@ abstract class Singleton
     public static function getInstance()
     {
         $cls = get_called_class(); // late-static-bound class name
-        if (!isset(self::$instances[$cls])) 
-            self::$instances[$cls] = new static;
-        
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+
         return self::$instances[$cls];
-    }   
+    }
 }
